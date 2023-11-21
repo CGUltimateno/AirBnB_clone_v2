2773a6c8b8957907be6f86917229f96f3b1690ec
CGUltimateno <Mohameddeaf65@gmail.com>
2023-11-21 18:41:38 +0200

    sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,17 +1,22 @@
 -- Set up the database and a user for the system
 
-CREATE DATABASE IF NOT EXIST hbnb_dev_db;
+-- Create database
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
+-- Create user
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
 IDENTIFIED BY 'hbnb_dev_pwd';
 
+-- Grant user hbnb_dev permissions to hbnb_dev_db
 GRANT ALL
 ON hbnb_dev_db.*
 TO 'hbnb_dev'@'localhost';
 
+-- Grant user read-only permission to performance_schema
 GRANT SELECT
 ON performance_schema.*
 TO 'hbnb_dev'@'localhost';
 
 
+-- Enforce new privileges
 FLUSH PRIVILEGES
