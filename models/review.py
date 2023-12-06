032e002e955015389c0a4a6049cd978a2cfd9fd7
CGUltimateno <Mohameddeaf65@gmail.com>
2023-12-06 08:37:12 -0800

    ÿes

--- models/review.py
+++ models/review.py
@@ -1,13 +1,1 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
-from sqlalchemy import Column, String, ForeignKey
-
-from models.base_model import BaseModel
-
-
-class Review(BaseModel):
-    """ Review class store review information """
-    __tablename__ = 'reviews'
-    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    text = Column(String(1024), nullable=False)
