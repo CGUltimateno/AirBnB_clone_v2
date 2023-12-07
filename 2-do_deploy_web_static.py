3877db378a6072061627c8b65211a305bd015992
CGUltimateno <Mohameddeaf65@gmail.com>
2023-12-07 08:43:34 +0200

    2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,26 +1,33 @@
 #!/usr/bin/python3
 """Fabric script that distributes an archive to your web servers, using the
 function do_deploy."""
+import os.path
 from fabric.api import put, run, env
-from os.path import exists
+
 env.hosts = ['52.23.177.244', '34.232.78.203']
 
 
 def do_deploy(archive_path):
     """Distributes an archive to your web servers."""
-    if exists(archive_path) is False:
+    if os.path.isfile(archive_path) is False:
         return False
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
     try:
         put(archive_path, "/tmp/")
-        file = archive_path.split("/")[-1]
-        folder = ("/data/web_static/releases/" + file.split(".")[0])
-        run("mkdir -p {}".format(folder))
-        run("tar -xzf /tmp/{} -C {}".format(file, folder))
+        run("mkdir -p /data/web_static/releases/{}".format(name))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}"
+            .format(file, name))
         run("rm /tmp/{}".format(file))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(folder, folder))
-        run("rm -rf {}/web_static".format(folder))
+        run("mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/".format(name, name))
+        run("rm -rf /data/web_static/releases/{}/web_static"
+            .format(name))
         run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(folder))
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(name))
         return True
+
     except:
         return False
+
