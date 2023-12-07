d25cf7d4f20d4d0245f8994583ba6fb1aa022857
CGUltimateno <Mohameddeaf65@gmail.com>
2023-12-07 09:08:32 +0200

    2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -14,20 +14,20 @@ def do_deploy(archive_path):
     file = archive_path.split("/")[-1]
     name = file.split(".")[0]
     try:
-        put(archive_path, "/tmp/{}".format(name))
-        run("mkdir -p /data/web_static/releases/{}".format(name))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}"
-            .format(file, name))
+        put(archive_path, "/tmp/{}".format(file))
+        run("rm -rf /data/web_static/releases/{}/".
+            format(name))
+        run("mkdir -p /data/web_static/releases/{}/".
+            format(name))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+            format(file, name))
         run("rm /tmp/{}".format(file))
-        run("mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/".format(name, name))
-        run("rm -rf /data/web_static/releases/{}/web_static"
-            .format(name))
+        run("mv /data/web_static/releases/{}/web_static/* "
+            "/data/web_static/releases/{}/".format(name, name))
+        run("rm -rf /data/web_static/releases/{}/web_static".
+            format(name))
         run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(name))
-        return True
-
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+            format(name))
     except:
         return False
-
