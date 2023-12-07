4ff78620310b90e97656eb0945fd75204600d81c
CGUltimateno <Mohameddeaf65@gmail.com>
2023-12-07 08:46:26 +0200

    2

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,29 +1,32 @@
 #!/usr/bin/env bash
 # Script that sets up your web servers for the deployment of web_static
 sudo apt-get update
-sudo apt-get -y install nginx
+sudo apt-get install -y nginx
 
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "$html" >  /data/web_static/releases/test/index.html
+echo "ALX" >  /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 chown -R ubuntu:ubuntu /data/
 chgrp -R ubuntu /data/
 
 printf %s "server{
-    listen 80;
+    listen 80 default_server;
     listen [::]:80 default_server;
     root   /var/www/html;
-    index index.html index.htm index.nginx-debian.html;
+    index index.html index.htm;
     add_header X-Served-By $HOSTNAME;
+
     location /hbnb_static {
         alias /data/web_static/current/;
         index index.html index.htm;
     }
+
     location /redirect_me {
         return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
     }
+
     error_page 404 /404.html;
     location /404 {
         root /var/www/html;
