7c131d4bc327a00bcd324b91e76642f268955f0f
CGUltimateno <Mohameddeaf65@gmail.com>
2023-12-28 20:00:56 +0200

    4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application with routes /, /hbnb, /c/<text>,
+/python<text>, and /number/<n>.
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """display Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """display HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c(text):
+    """display C followed by text"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/<text>')
+def python(text):
+    """display Python followed by text"""
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route('/python/')
+def python_default():
+    """display Python followed by text"""
+    return 'Python is cool'
+
+
+@app.route("/number/<num>")
+def number(n):
+    """display n is a number only if n is an integer"""
+    if n.isdigit():
+        return f"{int(n)} is a number"
+
+
+if __name__ == '__main__':
+    app.run(host="0")
