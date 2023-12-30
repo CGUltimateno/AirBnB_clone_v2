74c1852bfa682df90326ac92ceebf86bb82da12e
CGUltimateno <Mohameddeaf65@gmail.com>
2023-12-30 14:58:36 +0200

    4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-script that starts a Flask web application with routes /, /hbnb, /c/<text>,
+script that starts a Flask web application
+with routes /, /hbnb, /c/<text>,
 /python<text>, and /number/<n>.
 """
 from flask import Flask, abort
@@ -23,7 +24,7 @@ def hbnb():
 @app.route('/c/<text>')
 def c(text):
     """display C followed by text"""
-    return 'C {}'.format(text.replace('_', ' '))
+    return f"C {text.replace('_', ' ')}"
 
 
 @app.route('/python/<text>')
@@ -31,11 +32,10 @@ def python(text):
     """display Python followed by text"""
     return f"Python {text.replace('_', ' ')}"
 
-
 @app.route('/python/')
 def python_default():
     """display Python followed by text"""
-    return 'Python is cool'
+    return f'Python is cool'
 
 
 @app.route("/number/<num>")
